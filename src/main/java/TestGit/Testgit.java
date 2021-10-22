7d56cfdd33d48bb268c346581161e0e8d9aa834b
shenyizhi <461685179@qq.com>
2021-10-22 16:14:41 +0800

    master commit2

--- src/main/java/TestGit/Testgit.java
+++ src/main/java/TestGit/Testgit.java
@@ -7,5 +7,6 @@ public class Testgit {
         System.out.println("你好，沈轶智！");
         System.out.println("你好，苗宇梅！");
         System.out.println("你好，周彤彤！");
+        System.out.println("今天的天气真的挺好的呀！");
     }
 }
